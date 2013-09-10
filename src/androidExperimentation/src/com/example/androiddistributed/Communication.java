c884aaf53f18a1d0548d0fe5d1f3e4f8cde61a6d
theodori <theodori@ceid.upatras.gr>
2013-09-10 12:40:56 +0300

    SmartPhone Entity hibernate auto_inc on id

--- src/androidExperimentation/src/com/example/androiddistributed/Communication.java
+++ src/androidExperimentation/src/com/example/androiddistributed/Communication.java
@@ -1,6 +1,5 @@
 package com.example.androiddistributed;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.ksoap2.SoapEnvelope;
@@ -10,26 +9,22 @@ import org.ksoap2.serialization.SoapPrimitive;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
 import com.google.gson.Gson;
 
 import eu.smartsantander.androidExperimentation.jsonEntities.Plugin;
 import eu.smartsantander.androidExperimentation.jsonEntities.PluginList;
 import eu.smartsantander.androidExperimentation.jsonEntities.Smartphone;
 
-import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
-
 public class Communication extends Thread implements Runnable {
 
 	final String NAMESPACE = "http://androidExperimentation.smartsantander.eu/";
-	//final String URL = "http://83.212.115.57:8080/ADService/services/HelloWorld?wsdl"; 
 	final String URL = Constants.URL+":8080/services/AndroidExperimentationWS?wsdl";
 	
 	private Handler handler;
-	
-	// get TAG name for reporting to LogCat
 	private final String TAG = this.getClass().getSimpleName();
 	
 	public Communication(Handler handler)
@@ -103,18 +98,19 @@ public class Communication extends Thread implements Runnable {
 		int serverPhoneId = 0;
 		
 		Smartphone smartphone = new Smartphone(phoneId);
+		smartphone.setPhoneId(phoneId);
 		smartphone.setSensorsRules(sensorsRules);
-		//smartphone.setTimeRules("time_rules");		
-		Gson gson = new Gson();
-		String jsonSmartphone = gson.toJson(smartphone);		
-		String serverPhoneId_s = sendRegisterSmartphone(jsonSmartphone);		
+		String jsonSmartphone = (new Gson()).toJson(smartphone);		
+		String serverPhoneId_s ="";		
 		try
 		{
+			serverPhoneId_s = sendRegisterSmartphone(jsonSmartphone);
 			serverPhoneId = Integer.parseInt(serverPhoneId_s);
 		}
 		catch(Exception e)
 		{
 			serverPhoneId = Constants.PHONE_ID_UNITIALIZED;
+			Log.i(TAG, "Device Registration Exception:" + e.getMessage());
 		}		
 		return serverPhoneId;
 	}
@@ -122,26 +118,18 @@ public class Communication extends Thread implements Runnable {
 	private String sendRegisterSmartphone(String jsonSmartphone) throws Exception
 	{
 		Log.i(TAG, "send register smartphone");
-
 		final String METHOD_NAME = "registerSmartphone";
 		final String SOAP_ACTION = "\""+"http://AndroidExperimentationWS/registerSmartphone"+"\"";
-		
-		String serverPhoneId = "";
-		
+		String serverPhoneId = "";	
 		SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME); 
-
 		PropertyInfo propInfo=new PropertyInfo();
 		propInfo.name="arg0";
 		propInfo.type=PropertyInfo.STRING_CLASS;
 		propInfo.setValue(jsonSmartphone);
-  
-		request.addProperty(propInfo);  
-		
+		request.addProperty(propInfo);  		
 		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11); 
-		envelope.setOutputSoapObject(request);
-		
-		HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
-		
+		envelope.setOutputSoapObject(request);		
+		HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);		
 		try
 		{			
 			androidHttpTransport.call(SOAP_ACTION, envelope);			
