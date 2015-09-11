61bf3970ff55ee04d989bef15613ef2d770dbe09
mylonasg <mylonasg@ceid.upatras.gr>
2015-09-11 15:57:04 +0300

    small changes migrating to organicity
    small changes migrating to organicity UI

--- src/dynamix_framework/dynamix-framework/src/eu/smartsantander/androidExperimentation/tabs/reportTab.java
+++ src/dynamix_framework/dynamix-framework/src/eu/smartsantander/androidExperimentation/tabs/reportTab.java
@@ -16,14 +16,15 @@ public class reportTab extends Activity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reporter);
-        String text = "<html><body style=\"text-align:justify\" bgcolor=\"black\" text=\"white\"> %s </body></Html>";
+        String text = "<html><body style=\"text-align:justify\"  text=\"white\"> %s </body></Html>";
         
         WebView webView = (WebView) findViewById(R.id.webview1);
         
         //must add both "text/html; AND charset=utf-8" to the call to handle Greek and Spanish correctly in the WebView or
         //else it displays gibberish
-        
+        webView.setBackgroundColor(0);
         webView.loadData(String.format(text, getString(R.string.about1)), "text/html; charset=utf-8", "utf-8");
-                  
+        webView.setBackgroundResource(R.drawable.organicity_background);
+
     } 
 }
