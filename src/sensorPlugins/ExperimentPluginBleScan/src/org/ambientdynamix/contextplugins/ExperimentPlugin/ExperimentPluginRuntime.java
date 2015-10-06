8623e4bb1af261041d66d6e6bfa6b93f05f429b7
Dimitrios Amaxilatis <amaxilat@cti.gr>
2015-10-06 18:03:35 +0300

    updated plugins - now messages have a json format

---
+++ src/sensorPlugins/ExperimentPluginBleScan/src/org/ambientdynamix/contextplugins/ExperimentPlugin/ExperimentPluginRuntime.java
@@ -0,0 +1,96 @@
+package org.ambientdynamix.contextplugins.ExperimentPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.ambientdynamix.api.contextplugin.ContextPluginSettings;
+import org.ambientdynamix.api.contextplugin.PowerScheme;
+import org.ambientdynamix.api.contextplugin.ReactiveContextPluginRuntime;
+import org.ambientdynamix.api.contextplugin.security.PrivacyRiskLevel;
+import org.ambientdynamix.api.contextplugin.security.SecuredContextInfo;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import eu.smartsantander.androidExperimentation.jsonEntities.Reading;
+
+public class ExperimentPluginRuntime extends ReactiveContextPluginRuntime {
+
+	private final String TAG = this.getClass().getSimpleName();
+	private Context context;
+	private Bundle b;
+
+	@Override
+	public void init(PowerScheme powerScheme, ContextPluginSettings settings) throws Exception {
+		this.setPowerScheme(powerScheme);
+		this.context = this.getSecuredContext();
+		b = new Bundle();
+		Log.w(TAG, "ExperimentPluginNoiseGpsScan Inited!");
+	}
+
+	// handle incoming context request
+	@Override
+	public void handleContextRequest(UUID requestId, String contextType) {
+		Log.w(TAG, "ExperimentPluginNoiseGpsScan Workload Started!");
+		try {
+
+			String jsonGpsReading = b.getString("org.ambientdynamix.contextplugins.GpsPlugin");
+			String jsonBleReaderScan = b.getString("org.ambientdynamix.contextplugins.BleReaderPlugin");
+			List<Reading> readings = new ArrayList<Reading>();
+			PluginInfo info = new PluginInfo();
+			info.setState("ACTIVE");
+
+			if (jsonGpsReading != null) {
+				Log.w("Experiment Message:", jsonGpsReading);
+				readings.add(new Reading(Reading.Datatype.String, jsonGpsReading, PluginInfo.CONTEXT_TYPE));
+			}
+			if (jsonBleReaderScan != null) {
+				Log.w("Experiment Message:", jsonBleReaderScan);
+				readings.add(new Reading(Reading.Datatype.String, jsonBleReaderScan, PluginInfo.CONTEXT_TYPE));
+			}
+			info.setPayload(readings);
+			sendContextEvent(requestId, new SecuredContextInfo(info, PrivacyRiskLevel.LOW), 60000);
+		} catch (Exception e) {
+			Log.w("Experiment Workload Error", e.toString());
+		}
+	}
+
+	@Override
+	public void handleConfiguredContextRequest(UUID requestId, String contextType, Bundle config) {
+		b.clear();
+		b.putAll(config);
+		handleContextRequest(requestId, contextType);
+	}
+
+	@Override
+	public void start() {
+
+	}
+
+	@Override
+	public void stop() {
+
+	}
+
+	@Override
+	public void destroy() {
+		/*
+		 * At this point, the plug-in should stop and release any resources.
+		 * Nothing to do in this case except for stop.
+		 */
+		this.stop();
+		Log.d(TAG, "ExperimentPluginNoiseGpsScan Destroyed!");
+	}
+
+	@Override
+	public void updateSettings(ContextPluginSettings settings) {
+		// Not supported
+	}
+
+	@Override
+	public void setPowerScheme(PowerScheme scheme) {
+		// Not supported
+	}
+
+}
